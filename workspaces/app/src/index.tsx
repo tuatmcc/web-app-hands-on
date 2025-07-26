a5a3ff6adae9decd588fd3ac1a81cb49dbc44686
NISHIZAWA Shuntaro <me@s2n.tech>
2025-07-27 03:38:48 +0900

    feat: フロントエンドのセットアップ

---
+++ workspaces/app/src/index.tsx
@@ -0,0 +1,25 @@
+import "./global.css";
+
+import { createRouter, RouterProvider } from "@tanstack/react-router";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+import { routeTree } from "./routeTree.gen";
+
+const router = createRouter({ routeTree });
+
+declare module "@tanstack/react-router" {
+	interface Register {
+		router: typeof router;
+	}
+}
+
+const rootElement = document.getElementById("root");
+if (rootElement) {
+	const root = createRoot(rootElement);
+	root.render(
+		<StrictMode>
+			<RouterProvider router={router} />
+		</StrictMode>,
+	);
+}
