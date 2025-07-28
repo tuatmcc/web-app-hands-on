bac225451e30e7041996ba6499853c077290bcbb
NISHIZAWA Shuntaro <me@s2n.tech>
2025-07-29 02:31:12 +0900

    feat: 返信機能を追加

--- workspaces/app/src/components/PostForm.tsx
+++ workspaces/app/src/components/PostForm.tsx
@@ -1,39 +1,19 @@
-import { useMutation, useQueryClient } from "@tanstack/react-query";
 import type { ReactNode } from "react";
 import { useState } from "react";
-import { fetchClient } from "../libs/fetch-client";
 
-export function PostForm(): ReactNode {
+interface Props {
+	onSubmit: (name: string | undefined, content: string) => void;
+	isLoading: boolean;
+}
+
+export function PostForm({ onSubmit, isLoading }: Props): ReactNode {
 	const [name, setName] = useState("");
 	const [content, setContent] = useState("");
 
-	const query = useQueryClient();
-
-	const mutation = useMutation({
-		mutationFn: async () => {
-			const response = await fetchClient.api.posts.$post({
-				json: {
-					name: name || undefined,
-					content,
-				},
-			});
-
-			if (!response.ok) {
-				throw new Error("Failed to create post");
-			}
-
-			const data = await response.json();
-			return data;
-		},
-		onSuccess: () => {
-			query.invalidateQueries({ queryKey: ["posts"] });
-			setContent("");
-		},
-	});
-
 	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
-		mutation.mutate();
+		onSubmit(name || undefined, content);
+		setContent("");
 	};
 
 	return (
@@ -48,7 +28,7 @@ export function PostForm(): ReactNode {
 					className="w-full rounded-md border border-slate-400 bg-white p-2 outline-sky-400 outline-offset-4 disabled:bg-slate-100 disabled:text-slate-400"
 					value={name}
 					onChange={(e) => setName(e.target.value)}
-					disabled={mutation.isPending}
+					disabled={isLoading}
 				/>
 			</label>
 			<label className="flex w-full flex-col gap-1">
@@ -58,13 +38,13 @@ export function PostForm(): ReactNode {
 					rows={5}
 					value={content}
 					onChange={(e) => setContent(e.target.value)}
-					disabled={mutation.isPending}
+					disabled={isLoading}
 				/>
 			</label>
 			<button
 				type="submit"
 				className="rounded-full bg-sky-400 px-4 py-1 text-white disabled:bg-sky-300"
-				disabled={mutation.isPending}
+				disabled={isLoading}
 			>
 				投稿する
 			</button>
--- workspaces/app/src/routes/index.tsx
+++ workspaces/app/src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
 import { Post } from "../components/Post";
 import { PostForm } from "../components/PostForm";
@@ -23,9 +23,41 @@ function Page() {
 		},
 	});
 
+	const queryClient = useQueryClient();
+	const mutation = useMutation({
+		mutationFn: async ({
+			name,
+			content,
+		}: {
+			name: string | undefined;
+			content: string;
+		}) => {
+			const response = await fetchClient.api.posts.$post({
+				json: {
+					name,
+					content,
+				},
+			});
+
+			if (!response.ok) {
+				throw new Error("Failed to create post");
+			}
+
+			const data = await response.json();
+			return data;
+		},
+		onSuccess: () => {
+			queryClient.invalidateQueries({ queryKey: ["posts"] });
+		},
+	});
+
+	const handlePostSubmit = (name: string | undefined, content: string) => {
+		mutation.mutate({ name, content });
+	};
+
 	return (
 		<div className="flex flex-col gap-4">
-			<PostForm />
+			<PostForm onSubmit={handlePostSubmit} isLoading={mutation.isPending} />
 			<div className="flex flex-col gap-4">
 				{query.data?.posts.map((post) => (
 					<Post key={post.id} post={post} />
