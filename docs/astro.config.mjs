71f5f235b536be9e14e1847879ba48d5fa6418e9
NISHIZAWA Shuntaro <me@s2n.tech>
2025-07-30 08:15:46 +0900

    docs: はじめにを追加

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,24 +1,34 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+
+import starlight from "@astrojs/starlight";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			title: "MCC Webアプリ講習会資料",
+			locales: {
+				root: {
+					lang: "ja",
+					label: "日本語",
+				},
+			},
 			sidebar: [
 				{
-					label: 'Guides',
+					label: "はじめに",
+					slug: "",
+				},
+				{
+					label: "Guides",
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
+						{ label: "Example Guide", slug: "guides/example" },
 					],
 				},
 				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
+					label: "Reference",
+					autogenerate: { directory: "reference" },
 				},
 			],
 		}),
